45fcaf0438f49cfeaf19ea70fa342da0415def17
Marvin <43289257+MarvinZhong@users.noreply.github.com>
2022-04-28 16:04:39 +0800

    Create utilityCopy.py

---
+++ utilityCopy.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+
+parentFolder = "main/parent/folder"
+newParentFolder = "main/parent/newFolder"
+
+
+def allContentsToOneFolder():
+    for folderName, subfolders, filenames in os.walk(parentFolder):
+        print("The current folder is " + folderName)
+        for subfolder in subfolders:
+            print("SUB FOLDER OF " + folderName + ": " + subfolder)
+        for filename in filenames:
+            print("FILE INSIDE " + folderName + ": " + filename)
+            shutil.copy(os.path.join(folderName, filename), newParentFolder)
+
+
+def duplicateFolder():
+    # remake folder structure from parentFolder to newParentFolder
+    for folderName, subfolders, filenames in os.walk(parentFolder):
+        print("The current folder is " + folderName)
+        for subfolder in subfolders:
+            print("SUB FOLDER OF " + folderName + ": " + subfolder)
+        for filename in filenames:
+            print("FILE INSIDE " + folderName + ": " + filename)
+            # slice folderName without parentFolder
+            newFolderName = folderName[len(parentFolder):]
+            # make new folder
+            newFolder = newParentFolder + newFolderName
+            if not os.path.exists(newFolder):
+                os.makedirs(newFolder)
+            # copy file to new folder
+            shutil.copy(os.path.join(folderName, filename), newFolder)
+
+
+def simpleCopy():
+    shutil.copytree(parentFolder, newParentFolder)
+
+
+def main():
+    # copy the same folder structure and copy files to it
+    # simpleCopy()
+
+    # longer version of simpleCopy()
+    duplicateFolder()
+
+    # copy all contents to one folder
+    # allContentsToOneFolder()
+
+
+if __name__ == '__main__':
+    main()
