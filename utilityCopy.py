900b26bd89b0cb6ff0676c5208213010d43eb10f
Marvin <43289257+MarvinZhong@users.noreply.github.com>
2022-04-29 13:56:45 +0800

    Update utilityCopy.py
    with progress bar and counting files

--- utilityCopy.py
+++ utilityCopy.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from tqdm import tqdm
 
 parentFolder = "main/parent/folder"
 newParentFolder = "main/parent/newFolder"
@@ -18,11 +19,8 @@ def allContentsToOneFolder():
 def duplicateFolder():
     # remake folder structure from parentFolder to newParentFolder
     for folderName, subfolders, filenames in os.walk(parentFolder):
-        print("The current folder is " + folderName)
-        for subfolder in subfolders:
-            print("SUB FOLDER OF " + folderName + ": " + subfolder)
-        for filename in filenames:
-            print("FILE INSIDE " + folderName + ": " + filename)
+        print("\nThe current folder is " + folderName)
+        for filename in tqdm(filenames):
             # slice folderName without parentFolder
             newFolderName = folderName[len(parentFolder):]
             # make new folder
@@ -37,16 +35,42 @@ def simpleCopy():
     shutil.copytree(parentFolder, newParentFolder)
 
 
+def countFiles(countThisFolder):
+    listExt = []
+    for folderName, subfolders, filenames in os.walk(countThisFolder):
+        for filename in filenames:
+            # split filename to get extension
+            ext = os.path.splitext(filename)[1]
+            # if ext is not in listExt, add it
+            if ext not in listExt:
+                listExt.append(ext)
+    print("There are " + str(len(listExt)) + " different extensions in " + countThisFolder)
+    # print listExt
+    countTotal = 0
+    for ext in listExt:
+        count = 0
+        for folderName, subfolders, filenames in os.walk(countThisFolder):
+            for filename in filenames:
+                countTotal += 1
+                if filename.endswith(ext):
+                    count += 1
+        print("\tThere are " + str(count) + " files with extension " + ext + " in " + countThisFolder)
+    print("There are " + str(int(countTotal / len(listExt))) + " files in " + countThisFolder + "\n")
+
+
 def main():
-    # copy the same folder structure and copy files to it
+    # copy and replace the same folder structure and files to new folder
     # simpleCopy()
 
-    # longer version of simpleCopy()
-    # can't copy empty folder
+    # make the same folder structure and copy files to new folder without replacing
     duplicateFolder()
 
     # copy all contents to one folder
     # allContentsToOneFolder()
+
+    # count files with different ext in both folders
+    countFiles(parentFolder)
+    countFiles(newParentFolder)
 
 
 if __name__ == '__main__':
