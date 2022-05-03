2c3ab90fc32b1ef19bd15387bd1319e535b31a52
Marvin <43289257+MarvinZhong@users.noreply.github.com>
2022-05-03 13:47:56 +0800

    Update renameFiles.py

--- renameFiles.py
+++ renameFiles.py
@@ -3,18 +3,28 @@ import os
 # Function to rename multiple files
 resource = 'start/folder/with/files/to/rename'
 codename = "rename_"
+findExt = ".png"
+newExt = ".jpg"
 
 
 def main():
     for count, filename in enumerate(os.listdir(resource)):
         oriName, fileExt = os.path.splitext(filename)
         dst = codename + str(count+1) + fileExt
+        print("renaming " + filename + " to " + dst)
         src = resource + '/' + filename
         dst = resource + '/' + dst
 
         # rename() function will
         # rename all the files
         os.rename(src, dst)
+        
+        # change ext file without rename
+#         if filename.endswith(findExt):
+#             filename = os.path.splitext(filename)[0]
+#             print("changing file ext of ", filename)
+#             os.rename(os.path.join(resource, filename + findExt),
+#                       os.path.join(resource, filename + newExt))
 
 
 # Driver Code
